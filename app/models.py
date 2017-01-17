7d18750511ca5a8691b7b2b132956dcf2c592680
Avinash Jain <avinash.jain@booking.com>
2017-01-18 00:35:29 +0100

    refactorization

--- app/models.py
+++ app/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils import timezone
 from django.core.validators import RegexValidator, MinValueValidator
-import json
 
 class Grocery(models.Model):
 	name = models.CharField(
