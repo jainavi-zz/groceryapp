b4afb2dbf490d272ef8d1606acef29cb5fd6a5d6
Avinash Jain <avinash.jain@booking.com>
2017-02-22 23:51:11 +0100

    forgot password frontend implemented

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,13 +1,40 @@
 
-$('#singup-form').submit(function(e) {
+$('#form-singup').submit(function(e) {
 	e.preventDefault();
 	$.ajax({
 		type: 'POST',
 		url: '/signup',
-		data: $('#singup-form').serialize(),
+		data: $('#form-singup').serialize(),
 		dataType: 'json',
 		success: function(response) {
 			console.log(response)
 		}
 	});
+});
+
+$('.toggle').click(function(){
+	// Switches the Icon
+	$(this).children('i').toggleClass('fa-pencil');
+	// Switches the forms
+	$('.form').animate({
+		height: "toggle",
+		'padding-top': 'toggle',
+		'padding-bottom': 'toggle',
+		opacity: "toggle"
+	}, "slow");
+});
+
+$('.cta a').click(function(e) {
+	e.preventDefault();
+
+	$('#modal-forgot-password').modal({
+		backdrop: 'static',
+		keyboard: false
+	});
+	$('#modal-forgot-password').modal('show');
+});
+
+$('#form-forgot-password').submit(function(e) {
+	e.preventDefault();
+	console.log('Yahoo!');
 });
