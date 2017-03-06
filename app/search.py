6883c78d96bd2a25fbd020af3c9c9c7868d5d59b
Avinash Jain <avinash.jain@booking.com>
2017-03-06 18:03:38 +0100

    solr.config removed

--- app/search.py
+++ app/search.py
@@ -1,9 +1,15 @@
 __author__ = 'Avinash Jain'
 
+import os
 import pysolr
 
 def solr_search_items(query):
-	solr = pysolr.Solr('http://localhost:8983/solr/default/', timeout=10)
+	if 'SERVER_PROD' in os.environ:
+		solr_url = 'http://ec2-34-207-179-25.compute-1.amazonaws.com:8983/solr/default/'
+	else:
+		solr_url = 'http://localhost:8983/solr/default/'
+
+	solr = pysolr.Solr(solr_url, timeout=10)
 	results = solr.search(query, sort='id asc')
 
 	suggestions = []
