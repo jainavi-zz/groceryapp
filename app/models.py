f4cf99f2d699ef64ce17aeb646eb795556994fed
Avinash Jain <avinash.jain@booking.com>
2017-03-12 21:28:44 +0100

    Customer model replaced by UserdProfile

--- app/models.py
+++ app/models.py
@@ -50,26 +50,6 @@ class UserProfile(models.Model):
 	def __str__(self):
 		return str(self.__dict__)
 
-class Customer(models.Model):
-	name 		= models.CharField(max_length=50)
-	email 		= models.EmailField(max_length=70, unique=True)
-	phone 		= models.CharField(max_length=12, validators=[
-					RegexValidator(regex='^.{12}$', message='Invalid phone number')
-				], unique=True)
-	street 		= models.CharField(max_length=70)
-	house 		= models.CharField(blank=True, max_length=5)
-	city 		= models.CharField(max_length=50)
-	postal_code = models.CharField(max_length=10)
-	password	= models.CharField(max_length=256)
-
-	def clean(self):
-		for field in ['name', 'email', 'phone', 'street', 'city', 'postal_code']:
-			val = getattr(self, field)
-			if val: setattr(self, field, val.strip())
-
-	def __str__(self):
-		return str(self.__dict__)
-
 class ForgotPassword(models.Model):
 	access_token	= models.CharField(max_length=128, unique=True)
 	email 			= models.EmailField(max_length=70)
